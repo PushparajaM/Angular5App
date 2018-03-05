349ccb7d9bde12160f16aba067910c6b52ddad74
PushparajaM <pushparajacts@gmail.com>
2018-03-05 18:56:46 +0530

    package json new updates

--- src/server.js
+++ src/server.js
@@ -3,10 +3,4 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8000);
-
-app.get('/*', function(req, res) {
-    res.sendFile(path.json(__dirname + '/dist/index.html'));
-})
-
-console.log('8000 port is listening...')
+app.listen(process.env.PORT || 8080);
