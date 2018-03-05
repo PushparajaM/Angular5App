dfe536a1041db23a6dfa4662e7e46479d9431492
PushparajaM <pushparajacts@gmail.com>
2018-03-05 21:43:50 +0530

    server data updated comcs

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
-http.listen(process.env.PORT || 3000, function() {
+http.listen(process.env.PORT || 8080, function() {
     console.log('listening on', http.address().port);
 });
