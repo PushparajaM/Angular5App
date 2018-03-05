c7fd1ada6a0ef1bfc1e3aeefac9db8d0d6deb185
PushparajaM <pushparajacts@gmail.com>
2018-03-05 23:04:28 +0530

    Config changes New files added

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const http = require('http');
+const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-http.listen(process.env.PORT || 8005, function() {
-    console.log('listening on', http.address().port);
+app.use(express.static(path.join(__dirname + '/dist')));
+
+app.get('*', (res, req) => {
+    res.sendFile(path.join(__dirname + 'dist/index.html'));
 });
+const port = process.env.PORT || '3001';
+app.set('port', port)
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('Running'));
